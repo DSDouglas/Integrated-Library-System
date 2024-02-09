7a88a16fdc13d433ff843c2e9a6f69328a21e708
Fatman9000 <conman0503@gmail.com>
2024-02-09 13:31:54 -0500

    Not currently working. Reformatted file structure

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("./app/templates/index.html")
 
 @app.route("/login")
 def login():
