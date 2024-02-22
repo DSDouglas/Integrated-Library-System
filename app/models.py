906070756e6d5000ff7e001bc701bb47637b9fc7
Fatman9000 <conman0503@gmail.com>
2024-02-22 19:11:34 +0000

    Changed CartItem Model

--- app/models.py
+++ app/models.py
@@ -45,5 +45,3 @@ class CartItem(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
 
-    def __str__(self):
-        return f"{self.quantity} x {self.book.title}"
