d5b6ac1ed6c205b4ce478f8dff34ac92b5e6adcd
Fatman9000 <conman0503@gmail.com>
2024-02-27 13:46:40 -0500

    Authentication changes

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("catalog", views.catalog, name="catalog"),
-    path("login", views.login, name="login"),
+    path("login", views.login, include('django.contrib.auth.urls'), name="login"),
 ]
